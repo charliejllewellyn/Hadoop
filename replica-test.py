f6ca7ea70fe0fb345082f00436c5d39a54f2c0e4
serengeti <serengeti@172-2-0-117.lightspeed.dybhfl.sbcglobal.net>
2014-11-05 08:58:39 +0000

    Working code to return rack location of each block

--- replica-test.py
+++ replica-test.py
@@ -5,5 +5,8 @@ import re
 
 data = subprocess.Popen(["hdfs", "fsck", "/user/serengeti/", "-files", "-blocks", "-racks"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 out, err = data.communicate()
-for line in out.split('/default-rack/'):
-    print(line)
+#print(out)
+for line in out.split('\n'):
+    if re.match(r"^0. BP.*", line):
+        racks = re.match(r"0. BP.*\[.*(172\.2\.0\....):50010.*,.*(172\.2\.0\....):50010.*,.*(172\.2\.0\....):50010.*", line)
+        print(racks.group(1,2,3))
