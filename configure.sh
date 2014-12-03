61a04806d366c6b65c5221183a0ecadb63559b04
charliejllewellyn <charlie.j.llewellyn@gmail.com>
2014-12-03 16:39:56 +0000

    completed bind config

--- configure.sh
+++ configure.sh
@@ -6,6 +6,7 @@ hadoopDataNet=$2
 ownIp=`ifconfig | grep eth -A1 | tail -1 | awk '{print $2}' | awk -F: '{print $NF}'`
 date=`date +'%Y%m%dT%H:%M:%S'`
 logFile=/usr/local/bin/configure.log
+templateZoneFile=/usr/local/bin/zonefile.template
 
 # Function to run commands, check output and log
 function runCmdLog {
@@ -27,39 +28,47 @@ function configureFirewall {
 # Function to create named config files to provide DNS servivces for Hadoop
 function configureNamed {
         # Update named.conf to allow queries from hadoop subnets
-        ##sed -i "s=allow-query     { localhost; };=allow-query     { localhost; '$hadoopClientNet'; '$hadoopDataNet'; };=g" /etc/named.conf
+        sed -i 's=allow-query     { localhost; };=allow-query     { localhost; '$hadoopClientNet'; '$hadoopDataNet'; };=g' /etc/named.conf
+        grep '/etc/named/hadoop.zones' /etc/named.conf &>/dev/null
+        if [ $? != 0 ]; then
+                echo 'include "/etc/named/hadoop.zones";' >> /etc/named.conf
+        fi
         # Return first three octets of Hadoop address space so we can write out our addresses
         shortHadoopClientNet=`echo $hadoopClientNet | awk -F. '{print $1"."$2"."$3}'`
         shortHadoopDataNet1=`echo $hadoopDataNet | awk -F. '{print $1"."$2}'`
         shortHadoopDataNet2=`echo $hadoopDataNet | awk -F. '{print $3}'`
         count=1
-        # Create named zonefiles
-        cat /var/named/named.empty > /var/named/data/client.hadoop.local
-        sed -i "s/rname.invalid/client.hadoop.local/g" /var/named/data/client.hadoop.local
-        sed -i "s/3H/86400/g" /var/named/data/client.hadoop.local
-        cat /var/named/named.empty > /var/named/data/data.hadoop.local
-        sed -i "s/rname.invalid/data.hadoop.local/g" /var/named/data/data.hadoop.local
-        sed -i "s/3H/86400/g" /var/named/data/data.hadoop.local
+        # Create named zonefiles and configure named to read them
+        echo -e "zone \"client.hadoop.local\" IN {\n\ttype master;\n\tfile \"client.hadoop.local\";\n\tallow-update { none; };\n};" > /etc/named/hadoop.zones
+        echo -e "zone \"data.hadoop.local\" IN {\n\ttype master;\n\tfile \"data.hadoop.local\";\n\tallow-update { none; };\n};" >> /etc/named/hadoop.zones
+        revZone=`echo $hadoopClientNet | awk -F. '{print $3"."$2"."$1}'`
+        echo -e "zone \"$revZone.in-addr.arpa\" IN {\n\ttype master;\n\tfile \"$revZone\";\n\tallow-update { none; };\n};" >> /etc/named/hadoop.zones
+        cat $templateZoneFile > /var/named/client.hadoop.local
+        sed -i "s=TTL 3H=TTL 86400\n\$ORIGIN client.hadoop.local.=g" /var/named/client.hadoop.local
+        cat $templateZoneFile > /var/named/data.hadoop.local
+        sed -i "s=TTL 3H=TTL 86400\n\$ORIGIN data.hadoop.local.=g" /var/named/data.hadoop.local
+        cat $templateZoneFile > /var/named/$revZone
+        sed -i "s=TTL 3H=TTL 86400\n\$ORIGIN $revZone.IN-ADDR.ARPA.=g" /var/named/$revZone
         for num in {0..4}; do
-                zone=`echo $hadoopClientNet | awk -F. '{print $3+'$num'"."$2"."$1}'`
-                cat /var/named/named.empty > /var/named/data/$zone
-                sed -i "s/rname.invalid/ambari.client.hadoop.local/g" /var/named/data/$zone
-                sed "s=3H=86400\n\$ORIGIN $zone.IN-ADDR.ARPA.=g" /var/named/data/$zone
+                zone=`echo $hadoopDataNet | awk -F. '{print $3+'$num'"."$2"."$1}'`
+                echo -e "zone \"$zone.in-addr.arpa\" IN {\n\ttype master;\n\tfile \"$zone\";\n\tallow-update { none; };\n};" >> /etc/named/hadoop.zones
+                cat $templateZoneFile > /var/named/$zone
+                sed -i "s=TTL 3H=TTL 86400\n\$ORIGIN $zone.IN-ADDR.ARPA.=g" /var/named/$zone
         done
         # Populate named zone files
-        echo gateway    IN      A       $hadoopClientNet.254
-        echo 254        IN      PTR     gateway.client.hadoop.local.
-        echo ambari     IN      A       $hadoopClientNet.253
-        echo 253        IN      PTR     ambari.client.hadoop.local.
+        echo gateway    IN      A       $shortHadoopClientNet.254 >> /var/named/client.hadoop.local
+        echo 254        IN      PTR     gateway.client.hadoop.local. >> /var/named/`echo $hadoopClientNet | awk -F. '{print $3"."$2"."$1}'`
+        echo ambari     IN      A       $shortHadoopClientNet.253 >> /var/named/client.hadoop.local
+        echo 253        IN      PTR     ambari.client.hadoop.local. >> /var/named/`echo $hadoopClientNet | awk -F. '{print $3"."$2"."$1}'`
         for ip in {1..254}; do
-                ##echo client$count    IN      A       $shortHadoopClientNet1.
-                ##echo $ip       IN      PTR     client$count.client.hadoop.local.
+                echo client$count    IN      A       $shortHadoopClientNet.$ip >> /var/named/client.hadoop.local
+                echo $ip       IN      PTR     client$count.client.hadoop.local. >> /var/named/`echo $hadoopClientNet | awk -F. '{print $3"."$2"."$1}'`
                 count=`echo $count+1 | bc`
         done
         for num in {0..4}; do
                 for ip in {2..254}; do
-                ##      echo datanode$count    IN      A       $shortHadoopDataNet1.`echo $shortHadoopDataNet2+$num | bc`.$ip
-                ##      echo $ip        IN      PTR     client$count.data.hadoop.local.
+                        echo datanode$count    IN      A       $shortHadoopDataNet1.`echo $shortHadoopDataNet2+$num | bc`.$ip >> /var/named/data.hadoop.local
+                        echo $ip        IN      PTR     client$count.data.hadoop.local. >> /var/named/`echo $hadoopDataNet | awk -F. '{print $3+'$num'"."$2"."$1}'`
                         count=`echo $count+1 | bc`
                 done
         done
